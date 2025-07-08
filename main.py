01048ea105075bae852e69d145d42c388b289dea
SettyBhavithav <settybunny2006@gmail.com>
2025-07-08 16:51:34 +0530

    applied non maximum suppression to filter overlapping object boxes

--- main.py
+++ main.py
@@ -234,3 +234,5 @@ if __name__ == "__main__":
 # added requirements txt dependencies list with opencv python and pyttsx3
 
 # created opencv webcam video capture initialization script
+
+# applied non maximum suppression to filter overlapping object boxes
